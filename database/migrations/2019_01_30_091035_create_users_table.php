c3d538239e099801019a941c186b8d36e3f10812
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-03-13 11:09:36 +0000

    Поправь имя таблицы в миграции create_users_table

--- database/migrations/2019_01_30_091035_create_users_table.php
+++ database/migrations/2019_01_30_091035_create_users_table.php
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_collection');
+        Schema::dropIfExists('users');
     }
 }
