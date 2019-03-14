8aac83047246190ef780c4e7df8c09fbd63cc0fa
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-03-14 08:51:25 +0000

    Перенеси логику создания бекапа в библиотеку

---
+++ app/Console/Commands/BackupCreateCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Library\Backup\Create;
+use Illuminate\Console\Command;
+
+class BackupCreateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create backup';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Start creating backup');
+        $create = app(Create::class);
+        $this->info('Start dumping databases');
+        $create->dumpDatabase();
+        $this->output->success('Creating backup is successful');
+    }
+}
---
+++ app/Library/Backup/Create.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Library\Backup;
+
+class Create
+{
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = config('backup');
+    }
+
+    public function dumpDatabase()
+    {
+        $databases = $this->config['source']['databases'];
+        $temporaryPath = $this->config['temporaryDirectory'];
+        foreach ($databases as $db) {
+            $config = config("database.connections.${db}");
+            exec("mysqldump --user={$config['username']} --password={$config['password']} --host={$config['host']} {$config['database']} > {$temporaryPath}{$db}-dump.sql");
+        }
+    }
+}
