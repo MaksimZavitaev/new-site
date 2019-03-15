9aa16738932b5b74b996afdbd7e51351cfeffbb2
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-03-15 15:52:19 +0300

    Реализуй восстановление файлов и бекапа

--- app/Library/Backup/Restore.php
+++ app/Library/Backup/Restore.php
@@ -20,31 +20,48 @@ class Restore
 
     public function setFrom($path)
     {
-        $this->from = $path;
+        $this->from = realpath($path);
         return $this;
     }
 
     public function run()
     {
         $fs = new Filesystem();
-        $files = collect($fs->files($this->from));
-        foreach ($this->config['source']['databases'] as $db) {
-            $file = $this->from . $db . '.sql';
-            if ($fs->exists($file)) {
-                $config = config("database.connections.${db}");
-                $process = new Process(sprintf(
-                    'mysql --user=%s --password=%s --host=%s -v %s < %s',
-                    $config['username'],
-                    $config['password'],
-                    $config['host'],
-                    $config['database'],
-                    $file
-                ));
-                $process->start();
-                foreach ($process as $type => $data) {
-                    yield $data;
+        $files = collect($fs->allFiles($this->from));
+        foreach ($files as $file) {
+            /** @var SplFileInfo $file */
+            if ($file->getExtension() === 'sql') {
+                $db = $file->getBasename('.sql');
+                if ($config = config("database.connections.{$db}")) {
+                    $process = new Process(sprintf(
+                        'mysql --user=%s --password=%s --host=%s -v %s < %s',
+                        $config['username'],
+                        $config['password'],
+                        $config['host'],
+                        $config['database'],
+                        $file
+                    ));
+                    $process->start();
+                    foreach ($process as $type => $data) {
+                        yield $data;
+                    }
                 }
+                $fs->delete($file);
+                continue;
             }
+
+//            copy($file->getRealPath(), base_path($file->getRelativePathname()));
+            $path = base_path($file->getRelativePath());
+            $dest = $file->getRelativePathname();
+            if ($fs->exists($dest)) {
+                $fs->delete($dest);
+            }
+            if (!$fs->exists($path)) {
+                $fs->makeDirectory($path);
+            }
+            $fs->copy($file->getRealPath(), $dest);
+            yield "Copying file {$dest}";
+            $fs->delete($file);
         }
     }
 }
