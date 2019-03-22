be914182b1009b6480623e6ee64692742dd91b00
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-03-22 12:07:23 +0300

    Добавь хелпер для рекурсивного преобразования массива в объект

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -19,3 +19,16 @@ if (!function_exists('bytesToHuman')) {
         return round($bytes, 2) . ' ' . $units[$i];
     }
 }
+
+if (!function_exists('array2object')) {
+    function array2object($x)
+    {
+        if (!is_array($x)) {
+            return $x;
+        } elseif (is_numeric(key($x))) {
+            return array_map(__FUNCTION__, $x);
+        } else {
+            return (object)array_map(__FUNCTION__, $x);
+        }
+    }
+}
