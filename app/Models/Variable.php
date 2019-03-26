b4a852beef8733904e17920f532b543b2b0bf17c
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-03-26 14:38:57 +0300

    Добавь описание типов переменных

--- app/Models/Variable.php
+++ app/Models/Variable.php
@@ -14,4 +14,52 @@ class Variable extends Model
     protected $casts = [
         'data' => 'array',
     ];
+
+    protected $types = [
+        'string' => [
+            'value' => '',
+        ],
+        'text' => [
+            'value' => '',
+        ],
+        'link' => [
+            'href' => '',
+            'text' => '',
+            'blank' => false,
+        ],
+        'image' => [
+            'url' => '',
+            'size' => '',
+            'path' => '',
+        ],
+        'file' => [
+            'url' => '',
+            'size' => '',
+            'path' => '',
+            'update_at' => '',
+        ],
+    ];
+
+    /**
+     * Cast an attribute to a native PHP type.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    protected function castAttribute($key, $value)
+    {
+        $data = parent::castAttribute($key, $value);
+        if ($key === 'data') {
+            $fields = $this->types[$this->type];
+            foreach ($data as $k => $v) {
+                if (!isset($fields[$k])) {
+                    unset($data[$k]);
+                }
+            }
+            $data = array_merge($fields, $data);
+        }
+        return $data;
+    }
 }
