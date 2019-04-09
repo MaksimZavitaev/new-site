f6b209fe2ff23ed46e37ca88c51110ced629d7c3
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-04-09 13:49:10 +0300

    Добавь скоупы в модель Attention

--- app/Models/Attention.php
+++ app/Models/Attention.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Attention extends Model
 {
     public $timestamps = false;
+
+    public function scopeActive(Builder $query)
+    {
+        return $query->where('active', true);
+    }
+
+    public function scopeStarted(Builder $query)
+    {
+        return $query->where('started_at', '>', Carbon::now());
+    }
+
+    public function scopeNotEnded(Builder $query)
+    {
+        return $query->where('ended_at', '<', Carbon::now());
+    }
 }
