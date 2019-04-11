66f3fdbb8fbef706f8a4567da61d59695da9c631
Maksim Zavitaev <maksim.zavitaev@soglasie.ru>
2019-04-11 12:22:19 +0300

    Добавь миграции для создания необходимых таблиц

---
+++ database/migrations/2019_04_11_093201_create_offices_table.php
@@ -0,0 +1,91 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOfficesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('active');
+            $table->text('address');
+            $table->text('address_note')->nullable();
+            $table->string('lat');
+            $table->string('lon');
+            $table->json('seo');
+            $table->unsignedSmallInteger('sort');
+            $table->boolean('vip');
+            $table->boolean('main');
+            $table->boolean('master');
+            $table->boolean('card');
+            $table->boolean('delimobil');
+            $table->timestamps();
+        });
+
+        Schema::create('office_types', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name');
+        });
+
+        Schema::create('offices_types', function (Blueprint $table) {
+            $table->unsignedInteger('office_id');
+            $table->unsignedTinyInteger('office_type_id');
+            $table->json('schedule');
+            $table->json('phones');
+            $table->json('emails');
+
+            $table->foreign('office_id')
+                ->references('id')
+                ->on('offices')
+                ->onDelete('cascade');
+
+            $table->foreign('office_type_id')
+                ->references('id')
+                ->on('office_types')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('subways', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->unsignedTinyInteger('line');
+            $table->string('station');
+        });
+
+        Schema::create('offices_subways', function (Blueprint $table) {
+            $table->unsignedInteger('office_id');
+            $table->unsignedSmallInteger('subway_id');
+
+            $table->foreign('office_id')
+                ->references('id')
+                ->on('offices')
+                ->onDelete('cascade');
+
+            $table->foreign('subway_id')
+                ->references('id')
+                ->on('subways')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offices_subways');
+        Schema::dropIfExists('subways');
+        Schema::dropIfExists('offices_types');
+        Schema::dropIfExists('office_types');
+        Schema::dropIfExists('offices');
+    }
+}
